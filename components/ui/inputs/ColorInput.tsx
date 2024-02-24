f7b698c9bce73edb34c5d7341b48c1b27bd88181
Rami Bitar <rbitar@gmail.com>
2024-02-24 18:52:07 -0500

    Update color input

--- components/ui/inputs/ColorInput.tsx
+++ components/ui/inputs/ColorInput.tsx
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from 'react'
-import { Stack, Typography, Box, Slider } from '@mui/material'
+import { Stack, Tooltip, IconButton, Button, Typography, Box, Slider } from '@mui/material'
 import * as COLORS from '@mui/material/colors'
-import { TextInput } from '../../../components'
+import { Popup, Icon, TextInput } from '../../../components'
 import { SyntheticEvent } from '../../../types'
+import { useMenu } from '../../../hooks'
 import { MUI_COLORS, HEX_COLORS } from '../../../constants/index'
 
+type TransparentColorProps = {
+  value?: string
+  handleClick?: (e: any) => void
+}
+
+const TransparentColor: React.FC<TransparentColorProps> = (props) => {
+  const { value, handleClick } = props 
+  return(
+    <Box
+      sx={{
+        ...sx.color,
+        ...sx.transparent,
+        ...(value == '' && sx.selected),		
+        bgcolor: '#FFF',					
+      }}
+      onClick={handleClick}
+    />
+  )
+}
+
 type ColorInputProps = {
-	errors?: any
 	name: string
 	value: string
 	handleChange: (e: SyntheticEvent) => void
-	component?: any
+  errors?: any
 	disableTone?: boolean
 }
 
@@ -21,6 +41,8 @@ const ColorInput: React.FC<ColorInputProps> = (props) => {
   const [color, setColor] = useState(null)
 	const [hex, setHex] = useState(value || '')
   const [text, setText] = useState(value || '')
+
+  const { open, anchorEl, openMenu, closeMenu } = useMenu()
 
 	const handleToneChange = (event, newTone) => {
 		setTone(newTone)    
@@ -68,24 +90,45 @@ const ColorInput: React.FC<ColorInputProps> = (props) => {
   }, [hex])
 
 	return (
+    <Box sx={ sx.root }>      
+    <Button 
+      sx={ sx.button }
+      fullWidth
+      variant="outlined"
+      color="secondary"
+      endIcon={
+        <Stack direction='row' spacing={0}>
+          <Tooltip title={ value }>
+            <IconButton>
+              { value ? (
+              <Box
+                sx={{
+                  ...sx.color,	
+                  bgcolor: value,
+                }}
+              />
+              ):(
+                <TransparentColor 
+                  value={value}
+                  handleClick={ openMenu }
+                />
+              )}
+            </IconButton>
+          </Tooltip>  
+          <IconButton size="small">
+            <Icon name="ChevronDown" size={20} />
+          </IconButton>
+        </Stack>          
+      }
+      onClick={openMenu}>          
+      Choose color
+    </Button>      
+    <Popup 
+      open={open}
+      anchorEl={anchorEl}
+      handleClose={closeMenu}
+    >
 		<Stack spacing={2} direction="column" sx={sx.root}>
-			{!disableTone && (
-				<Stack spacing={0} sx={sx.slider}>
-					<Typography variant="caption" color="textSecondary">
-						Tone
-					</Typography>
-					<Slider
-						aria-label="Tone"
-						defaultValue={[100, 900]}
-						valueLabelDisplay="auto"
-						onChange={handleToneChange}
-						step={100}
-						min={100}
-						max={900}
-						value={tone}
-					/>
-				</Stack>
-			)}
 			<Box sx={sx.grid}>
 				{MUI_COLORS.map((color) => (
 					<Box
@@ -107,16 +150,28 @@ const ColorInput: React.FC<ColorInputProps> = (props) => {
 						onClick={() => handleHexColorChange(hexColor)}
 					/>
 				))}
-        	<Box
-						sx={{
-              ...sx.color,
-							...sx.transparent,
-							...(hex == '' && sx.selected),		
-              bgcolor: '#FFF',					
-						}}
-						onClick={() => handleHexColorChange('')}
-					/>
+        <TransparentColor 
+          value={hex}
+          handleClick={() => handleHexColorChange('')}
+        />        
 			</Box>
+      {!disableTone && (
+				<Stack spacing={0} sx={sx.slider}>
+					<Typography variant="caption" color="textSecondary">
+						Color tone
+					</Typography>
+					<Slider
+						aria-label="Tone"
+						defaultValue={[100, 900]}
+						valueLabelDisplay="auto"
+						onChange={handleToneChange}
+						step={100}
+						min={100}
+						max={900}
+						value={tone}
+					/>
+				</Stack>
+			)}
       <Box sx={sx.input}>
         <TextInput 
           name={name}
@@ -125,6 +180,8 @@ const ColorInput: React.FC<ColorInputProps> = (props) => {
         />
       </Box>
 		</Stack>
+  </Popup>
+</Box>
 	)
 }
 
@@ -135,6 +192,10 @@ const sx = {
 		width: '100%',
 		mb: 2,
 	},
+  button: {
+    py: 0,
+    justifyContent: 'space-between',
+  },
 	grid: {
 		display: 'grid',
 		gridTemplateColumns: 'repeat(7, 1fr)',
