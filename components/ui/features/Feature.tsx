64526c17be902357a8329be1bbf7abe0ac82a207
Rami Bitar <rbitar@gmail.com>
2024-03-04 14:20:41 -0500

    Update import

--- components/ui/features/Feature.tsx
+++ components/ui/features/Feature.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon } from 'frontend-ui/components'
+import { Icon } from '../../../components'
 import { Stack, Avatar, Typography } from '@mui/material'
 
 type FeatureProps = {
