92d76503e8e84ac633832150588e48deb7b507e6
Rami Bitar <rbitar@gmail.com>
2024-06-03 08:59:54 -0400

    update shadows

--- theme/shadows.ts
+++ theme/shadows.ts
@@ -1,8 +1,9 @@
 export const shadows = [
+  'none',
   '0 0 10px 0 rgba(0, 0, 0, 0.1)',
   '0 0 10px 0 rgba(0, 0, 0, 0.2)',
   '0 0 10px 0 rgba(0, 0, 0, 0.3)',
   '0 0 10px 0 rgba(0, 0, 0, 0.4)',
   '0 0 10px 0 rgba(0, 0, 0, 0.5)',
-  ...Array(20).fill('none')
+  ...Array(19).fill('none')
 ]
