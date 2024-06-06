a05acd0785d69897bdb5334481d3ece1c990f60b
Rami Bitar <rbitar@gmail.com>
2024-06-06 18:59:49 -0400

    use console logging

--- components/ui/inputs/LocationInput.tsx
+++ components/ui/inputs/LocationInput.tsx
@@ -1,45 +1,120 @@
-import React from "react";
-import { GooglePlacesInput } from "../../../components";
+import React, { useEffect, useState } from "react";
+import { useGooglePlaces } from "../../../hooks";
+import { TextInput } from '../../../components'
+import { TextInputPropsType } from '../../../types'
+import { useDebounce } from 'use-debounce'
+import { 
+  Stack,
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 
-type LocationInputProps = {
-  name?: string
-  handleChange: (ev: any) => void
-  value?: string
-  label?: string
-  placeholder?: string
-  direction?: "row" | "column"
-}
-
-const LocationInput: React.FC<LocationInputProps> = (props) => {
+const LocationInput: React.FC<TextInputPropsType> = (props) => {
   
   const { 
     name='location', 
-    handleChange, 
-    value,
+    value='', 
     label,
-    placeholder,
-    direction 
+    placeholder="Search location",
+    handleChange, 
+    direction='column' 
   } = props || {};
 
-  const handlePlaceChange = (place) => {
+  const { 
+    loading, 
+    places, 
+    fetchPlaces 
+  } = useGooglePlaces();
+
+  const [keywords, setKeywords] = useState(value)
+	const [debouncedValue] = useDebounce(keywords, 500)
+
+  const handleKeywordChange = (ev) => {
+    const { value } = ev.target;
+    setKeywords(value)
+    if(options?.length > 0) setOpen(true);
+  };
+
+  const handleClick = (option) => {
+    setOpen(false)
+    setKeywords(option?.value)
     handleChange({
       target: {
-        name,
-        value: place.value
-      }
+        name: name,
+        value: option?.value,
+      },
     })
-  }
+  }  
+
+  useEffect(() => {
+    fetchPlaces(debouncedValue);
+  }, [debouncedValue])
+
+  const [open, setOpen] = useState(false)
+  const [options, setOptions] = useState([]);
+  
+  useEffect(() => {
+    if (places?.length > 0) {
+      setOptions(places?.map((place) => ({ 
+        label: place?.formattedAddress, 
+        value: place?.formattedAddress
+      })));
+    }else{
+      setOpen(false)      
+    }
+  }, [places])
 
   return (
-    <GooglePlacesInput  
-      handleChange={ handlePlaceChange }
-      name={ name }
-      value={ "801 S Miami Ave" }
-      label={ label }
-      placeholder={ placeholder }
-      direction={ direction }
-    />
+    <Stack width={'100%'} direction="column">
+      <TextInput 
+        name={name}
+        label={label}
+        value={keywords}
+        options={options}
+        handleChange={handleKeywordChange}
+        direction={direction}
+        placeholder={ placeholder }
+      />
+      { open && (
+      <Box sx={sx.container}>
+        <List 
+          dense 
+          disablePadding
+          sx={ sx.list }
+        >
+        { options?.map((option, index) => (
+          <ListItem disableGutters>
+            <ListItemButton sx={ sx.listItemButton } onClick={() => handleClick(option) }>
+              <ListItemText primary={option.label} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+        </List>
+      </Box>
+      )}
+    </Stack>
   );
-}
-
+};
 export default LocationInput;
+
+const sx = {
+  container: {
+    position: 'relative',
+    width: '100%',
+  },
+  list: {
+    bgcolor: 'background.paper',    
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    zIndex: 1
+  },
+  listItemButton: {
+    px: 1,
+  }
+}
