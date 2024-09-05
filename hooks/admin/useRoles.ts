bd1ef316a1e112672698f280f4f26ec0c0919b0c
Rami Bitar <rbitar@gmail.com>
2024-09-05 12:02:02 -0400

    move editor components

---
+++ hooks/admin/useRoles.ts
@@ -0,0 +1,73 @@
+import React from 'react'
+import { useResource } from 'frontend-js'
+import { useAdmin } from '../../hooks'
+
+
+const useRoles = () => {
+	const { apiUrl } = useAdmin()
+
+	const {
+		errors,
+		loading,
+		loaded,
+		empty,
+		editing,
+		isValid,
+		resource: role,
+		resources: roles,
+		setResource: setRole,
+		findOne: findRole,
+		findMany: findRoles,
+		update: updateRole,
+		create: createRole,
+		save: saveRole,
+		destroy: deleteRole,
+		handleChange,
+		handleChangePage,
+		reloadOne: reloadRole,
+		reloadMany: reloadRoles,
+		query,
+		page,
+		numPages,
+		perPage,
+		totalCount,
+		sortBy,
+		sortDirection,
+		handleSort,
+	} = useResource({
+		url: `${apiUrl}/roles`,
+		name: 'role',
+	})
+
+	return {
+		errors,
+		loading,
+		loaded,
+		empty,
+		editing,
+		isValid,
+		role,
+		roles,
+		setRole,
+		findRole,
+		findRoles,
+		saveRole,
+		updateRole,
+		createRole,
+		deleteRole,
+		handleChange,
+		handleChangePage,
+		query,
+		page,
+		numPages,
+		perPage,
+		totalCount,
+		reloadRole,
+		reloadRoles,
+		sortBy,
+		sortDirection,
+		handleSort,
+	}
+}
+
+export default useRoles
