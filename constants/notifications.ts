208fced9438be29e103b6fb306c43171fd34ccaa
Rami Bitar <rbitar@gmail.com>
2024-10-05 17:56:09 -0400

    automated update

--- constants/notifications.ts
+++ constants/notifications.ts
@@ -1,19 +1,6 @@
-const DEFAULT_NOTIFICATION_OPTIONS = [
+export const NOTIFICATION_OPTIONS = [
 	{ value: 'text', label: 'Text', icon: 'CircleAlert' },
 	{ value: 'page', label: 'Page', icon: 'StickyNote' },
 	{ value: 'url', label: 'URL', icon: 'ExternalLink' },
-	{ value: 'document', label: 'Document', icon: 'Database' },
 ]
 
-export const NOTIFICATION_OPTIONS = {
-	default: DEFAULT_NOTIFICATION_OPTIONS,
-	shopify: [
-		...DEFAULT_NOTIFICATION_OPTIONS,
-		{
-			value: 'shopify_collection',
-			label: 'Shopify List',
-			icon: 'ShoppingCart',
-		},
-		{ value: 'shopify_product', label: 'Shopify Product', icon: 'TShirt' },
-	],
-}
