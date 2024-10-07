e972ee6c4e2c91f1d168dcbdaed7faf04dceb8f9
Rami Bitar <rbitar@gmail.com>
2024-10-07 11:56:22 -0400

    update heading

--- components/ui/typography/Heading.tsx
+++ components/ui/typography/Heading.tsx
@@ -1,114 +1,65 @@
 import React from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import { Stack, Typography } from '@mui/material'
 import { TypographyVariantsType } from '../../../types'
+import { cn } from '../../../shadcn/lib/utils'
 
 type HeadingProps = {
-	label?: string
-	title?: string
-	description?: string
-	textAlign?: 'left' | 'center'
-	textVariant?: TypographyVariantsType
-	enableBorder?: boolean
-	secondaryAction?: React.ReactNode
+  label?: string
+  title?: string
+  description?: string
+  textAlign?: 'left' | 'center'
+  textVariant?: TypographyVariantsType
+  enableBorder?: boolean
+  secondaryAction?: React.ReactNode
 }
 
 const Heading: React.FC<HeadingProps> = (props) => {
-	const {
-		label,
-		title,
-		description,
-		textAlign = 'left',
-		textVariant = 'h4',
-		enableBorder = false,
-		secondaryAction,
-	} = props || {}
+  const {
+    label,
+    title,
+    description,
+    textAlign = 'left',
+    textVariant = 'h4',
+    secondaryAction,
+  } = props || {}
 
-	if (!title && !description && !label) return null
-	return (
-		<Box sx={sx.root}>
-			<Stack sx={sx.toolbar} direction={{ sm: 'row', xs: 'column' }}>
-				<Stack
-					sx={{
-						...sx.stack,
-						...(enableBorder && sx.border),
-						alignItems: textAlign,
-					}}
-					direction={'column'}
-					spacing={0.5}
-				>
-					{label && (
-						<Typography variant="caption" color="text.secondary">
-							{label}
-						</Typography>
-					)}
-					{title && (
-						<Typography
-							variant={textVariant}
-							color="text.primary"
-							sx={{
-								...sx.title,
-								textAlign: textAlign,
-							}}
-						>
-							{title}
-						</Typography>
-					)}
-					{description && (
-						<Typography
-							variant="body1"
-							color="text.secondary"
-							sx={{
-								...sx.description,
-								textAlign: textAlign,
-							}}
-						>
-							{description}
-						</Typography>
-					)}
-				</Stack>
-				{secondaryAction}
-			</Stack>
-		</Box>
-	)
+  if (!title && !description && !label) return null
+  return (
+    <div className="p-4 w-full flex justify-between items-center flex-col sm:flex-row">
+      <Stack 
+        className="w-full justify-between"        
+      >
+        <Stack
+          direction={'column'}
+          spacing={0}
+          alignItems={textAlign}
+        >
+          {label && (
+            <Typography variant="caption" color="text.secondary">
+              {label}
+            </Typography>
+          )}
+          {title && (
+            <Typography
+              variant={textVariant}              
+              textAlign={textAlign}              
+            >
+              {title}
+            </Typography>
+          )}
+          {description && (
+            <Typography
+              variant="body1"              
+              textAlign={textAlign}              
+            >
+              {description}
+            </Typography>
+          )}
+        </Stack>
+        {secondaryAction}
+      </Stack>
+    </div>
+  )
 }
 
 export default Heading
-
-const sx = {
-	root: {
-		width: '100%',
-		display: 'flex',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		flexDirection: {
-			xs: 'column',
-			sm: 'row',
-		},
-	},
-	stack: {
-		pb: 1,
-		width: '100%',
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	border: {
-		py: 2,
-		borderBottom: '1px solid',
-		borderColor: 'divider',
-	},
-	title: {
-		width: '100%',
-		'& span': {
-			color: 'primary.main',
-		},
-	},
-	description: {
-		width: '100%',
-		whiteSpace: 'pre-line',
-	},
-	toolbar: {
-		width: '100%',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-	},
-}
