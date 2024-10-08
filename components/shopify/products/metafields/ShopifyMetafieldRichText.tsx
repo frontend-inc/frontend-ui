951843528871fc04d8ccef8d124ad6f8ae024495
Rami Bitar <rbitar@gmail.com>
2024-10-08 17:05:54 -0400

    fix import

--- components/shopify/products/metafields/ShopifyMetafieldRichText.tsx
+++ components/shopify/products/metafields/ShopifyMetafieldRichText.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { cn } from "../../../shadcn/lib/utils"
+import { cn } from "../../../../shadcn/lib/utils"
 
 type ShopifyMetafieldRichTextProps = {
   value: string
