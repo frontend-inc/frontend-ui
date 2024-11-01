fa7a528daf1e54700dee1769794170483b71668b
Rami Bitar <rbitar@gmail.com>
2024-10-31 21:08:01 -0400

    automated update

--- components/social/product-favorites/ProductFavoriteButton.tsx
+++ components/social/product-favorites/ProductFavoriteButton.tsx
@@ -52,7 +52,7 @@ export default function ProductFavoriteButton({
 				className={cn(
 					size === 'large' &&
 						'border border-divider bg-background text-secondary hover:bg-background hover:text-secondary',
-					isFavorite && 'text-primary hover:text-primary-dark border-primary ring-offset-2 ring-2 ring-ring',
+					isFavorite && 'text-primary hover:text-primary-dark',
 				)}
 			>
 				<Bookmark
