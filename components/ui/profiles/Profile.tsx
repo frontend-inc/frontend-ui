ed0326c955e31ca71d967cdb34901c2f01aaba9f
Rami Bitar <rbitar@gmail.com>
2024-11-13 20:52:00 -0500

    automated update

--- components/ui/profiles/Profile.tsx
+++ components/ui/profiles/Profile.tsx
@@ -1,16 +1,16 @@
 'use client'
 
 import React from 'react'
-import { AvatarImage, Label, ExpandableText, SocialLink } from '../..'
-import { Typography } from '../../core'
+import { AvatarImage, SocialLink } from '../..'
+import { Text } from '../../../components'
 import { cn } from 'frontend-shadcn'
 
 export type ProfileProps = {
 	image: string
 	label?: string
 	title: string
-	subtitle?: string
-  description?: string
+	subtitle: string
+  description: string
 	socialLinks?: {
 		provider: string
 		url: string
@@ -22,7 +22,7 @@ const Profile: React.FC<ProfileProps> = (props) => {
 
 	return (
 		<div className="w-full flex flex-col items-center justify-center rounded">
-			<div className="w-full max-w-[600px] flex flex-col space-y-4 items-start  sm:flex-row sm:space-x-6 sm:space-y-0">
+			<div className="w-full max-w-screen-md flex flex-col space-y-4 items-start sm:flex-row sm:space-x-[40px] sm:space-y-0">
 				<div className="flex flex-col items-center space-y-1">
 					<div className="h-[200px] w-[200px]">
 						<AvatarImage alt={title} src={image} size={200} />
@@ -36,10 +36,12 @@ const Profile: React.FC<ProfileProps> = (props) => {
 					</div>
 				</div>
 				<div className="flex flex-col space-y-1 w-full sm:w-auto">
-					<Label label={label} />
-					<Typography variant="h4">{title}</Typography>
-          <Typography variant="subtitle2">{subtitle}</Typography>
-					{description && <ExpandableText text={description} />}
+          <Text 
+            label={ label }
+            title={ title }
+            subtitle={ subtitle }
+            description={ description }
+          />          
 				</div>
 			</div>
 		</div>
