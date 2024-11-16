5573de593b7cede6e9b7aedbfe2da951e3da28f7
Rami Bitar <rbitar@gmail.com>
2024-11-16 10:33:23 -0500

    automated update

--- components/ui/typography/Quote.tsx
+++ components/ui/typography/Quote.tsx
@@ -19,22 +19,26 @@ const Quote: React.FC<QuoteProps> = (props) => {
 	} = props || {}
 
 	return (
-    <figure className={cn("max-w-screen-md mx-auto text-center", className)}>
-    <RiDoubleQuotesL size={36} className="text-foreground/50" />    
-    <blockquote>
-      <p className="text-2xl italic font-medium text-foreground">
-        {text}
-      </p>
-    </blockquote>
-    {author && (
-      <figcaption className="flex items-center justify-center mt-6 space-x-3">
-        <div className="flex items-center divide-x-2 divide-border">
-          <cite className="pr-3 font-medium text-foreground">
-            {author}
-          </cite>
-        </div>
-      </figcaption>
-    )}
+    <figure className={cn("flex space-x-4", className)}>
+      <div className="w-[36px] h-[36px] flex items-center justify-center">      
+        <RiDoubleQuotesL size={36} className="text-muted-foreground" />    
+      </div>
+    <div className="flex flex-col space-y-4">
+      <blockquote>
+        <p className="text-xl italic font-medium text-muted-foreground leading-loose">
+          {text}
+        </p>
+      </blockquote>
+      {author && (
+        <figcaption className="flex items-center justify-center mt-6 space-x-3">
+          <div className="flex items-center divide-x-2 divide-border">
+            <cite className="pr-3 font-medium text-muted-foreground">
+              &mdash; {author}
+            </cite>
+          </div>
+        </figcaption>
+      )}
+    </div>
   </figure>
 
 	)
