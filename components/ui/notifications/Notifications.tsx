f164efd72e10e7fcba59d8368d195b052e533a34
Rami Bitar <rbitar@gmail.com>
2024-12-04 15:48:04 -0500

    automated update

--- components/ui/notifications/Notifications.tsx
+++ components/ui/notifications/Notifications.tsx
@@ -1,7 +1,13 @@
 'use client'
 
 import React, { useState } from 'react'
-import { Swipeable } from '../../../components'
+import { 
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious  
+} from 'frontend-shadcn'
 import { NotificationType } from '../../../types'
 import { Notification } from '../../../components'
 
@@ -12,14 +18,19 @@ type NotificationsProps = {
 export default function Notifications({ notifications }: NotificationsProps) {
 
 	if (!notifications?.length) return null
-
 	return (
 		<div className="bg-primary w-full relative top-0 left-0 p-0 pb-[30px] h-11 px-[48px]">
-			<Swipeable enableArrows>
+			<Carousel>
+        <CarouselContent>
 				{notifications.map((notification, i) => (
-					<Notification key={i} notification={notification} />
+          <CarouselItem key={i}>
+					  <Notification key={i} notification={notification} />
+          </CarouselItem>
 				))}
-			</Swipeable>			
+        </CarouselContent>
+        <CarouselPrevious className="h-8 w-8 ml-2 border-0 bg-primary hover:bg-black/10 text-primary-foreground hover:text-primary-foreground" />
+        <CarouselNext className="h-8 w-8 mr-2 border-0 bg-primary hover:bg-black/10 text-primary-foreground hover:text-primary-foreground" />
+			</Carousel>			
 		</div>
 	)
 }
