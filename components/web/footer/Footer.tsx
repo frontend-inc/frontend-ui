4da38f60b596293f20a32bf7c4e03fcdac16a259
Rami Bitar <rbitar@gmail.com>
2024-12-16 14:43:05 -0500

    automated update

--- components/web/footer/Footer.tsx
+++ components/web/footer/Footer.tsx
@@ -26,7 +26,7 @@ const Footer: React.FC<FooterProps> = (props) => {
 	} = props
 
 	return (
-		<div className="flex flex-col w-full bg-background min-h-[80px]">
+		<div className="flex flex-col w-full bg-background min-h-[80px] pb-4">
       <div className="flex flex-col space-y-3 w-full items-center justify-center">
         <div className="flex flex-wrap flex-col space-y-2 sm:space-y-0 sm:flex-row sm:space-x-3 items-center justify-center">
           {links?.map((menuLink, i) => (
