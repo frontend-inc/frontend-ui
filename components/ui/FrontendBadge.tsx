9d410a6c65c7dba907db08af78a1a16cf2b927d4
Rami Bitar <rbitar@gmail.com>
2024-12-19 22:42:13 -0500

    automated update

--- components/ui/FrontendBadge.tsx
+++ components/ui/FrontendBadge.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 const FrontendBadge: React.FC = () => {
   return (
-    <div className="absolute bottom-0 right-0 w-full flex flex-row justify-end pb-2 pr-2">
+    <div className="absolute bottom-0 right-0 w-full flex flex-row justify-end pb-4 pr-4">
       <Link 
         href="https://www.frontend.co" 
         target="_blank" 
-        className="relative p-[1px] rounded-full shadow-lg hover:scale-105 transition-transform cursor-pointer overflow-hidden"
+        className="relative p-[2px] rounded-full shadow-lg hover:scale-105 transition-transform cursor-pointer overflow-hidden"
       >
         <div className="absolute inset-0 bg-gradient-to-r from-violet-500 via-sky-500 to-rose-500"></div>
         <div className="relative text-xs bg-gradient-to-br from-black to-zinc-800 text-white px-4 py-2 font-medium rounded-full">
