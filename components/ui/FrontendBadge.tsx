84245120aa7ca925a52eb4f2eddb1806ad011629
Rami Bitar <rbitar@gmail.com>
2024-12-23 13:45:42 -0500

    automated update

--- components/ui/FrontendBadge.tsx
+++ components/ui/FrontendBadge.tsx
@@ -9,9 +9,12 @@ const FrontendBadge: React.FC = () => {
         target="_blank" 
         className="relative p-[2px] rounded-full shadow-lg hover:scale-105 transition-transform cursor-pointer overflow-hidden"
       >
-        <div className="absolute inset-0 bg-gradient-to-r from-violet-500 via-sky-500 to-rose-500"></div>
-        <div className="relative text-xs bg-gradient-to-br from-black to-zinc-800 text-white px-4 py-2 font-medium rounded-full">
-          Built with Frontend
+        <div className="inline-block p-[2px] rounded-full bg-gradient-to-r from-purple-500 via-violet-500 to-blue-500">
+          <div className="px-3 py-1 rounded-full bg-background">
+            <span className="text-sm font-bold bg-gradient-to-r from-purple-500 via-violet-500 to-blue-500 text-transparent bg-clip-text">
+              Built with Frontend
+            </span>
+          </div>
         </div>
       </Link>
     </div>
--- components/web/testimonials/TestimonialCard.tsx
+++ components/web/testimonials/TestimonialCard.tsx
@@ -27,7 +27,7 @@ const TestimonialCard: React.FC<TestimonialProps> = (props) => {
 			<div className="flex flex-col space-y-3 justify-between h-full">
 				<div>
 					{text && (
-						<Typography variant="body1" className="font-normal leading-loose">
+						<Typography variant="subtitle1" className="font-normal leading-loose">
 							{text}
 						</Typography>
 					)}
