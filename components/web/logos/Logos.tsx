716c1456b0a9c1f7946385168500b513f3c1ba87
Rami Bitar <rbitar@gmail.com>
2025-01-06 21:18:52 -0500

    automated update

--- components/web/logos/Logos.tsx
+++ components/web/logos/Logos.tsx
@@ -42,8 +42,8 @@ const Logos: React.FC<LogosProps> = (props) => {
 						</div>
 					))}
 				</Marquee>
-				<div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-				<div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
+				<div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-backround dark:from-background"></div>
+				<div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-background dark:from-background"></div>
 			</div>
 			{logos?.length === 0 && (
 				<Empty title="No logos" description="Logos will appear here" />
