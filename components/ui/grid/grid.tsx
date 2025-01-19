59cc37e6e876f1a482fda5edc2134171d7fa3384
Rami Bitar <rbitar@gmail.com>
2025-01-19 12:39:16 -0500

    automated update

--- components/ui/grid/grid.tsx
+++ components/ui/grid/grid.tsx
@@ -8,7 +8,7 @@ const Grid: React.FC<GridProps> = (props) => {
 
   const { children } = props || {}
   return(
-    <div className="grid grid-cols-1 gap-4 auto-rows-[50px] md:grid-cols-12">
+    <div className="grid grid-cols-1 gap-1 md:gap-4 auto-rows-[50px] md:grid-cols-12">
       { children }
     </div>
   )
